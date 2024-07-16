d2164eeeec117d95991598e31c2f5c167b68903d
jeflynn135 <jenna.flynn135@gmail.com>
2024-07-15 22:23:09 -0700

    Added coding Questions

--- questions.js
+++ questions.js
@@ -1,22 +1,27 @@
 var questions = [
   {
-    title: "Question 1",
-    choices: ["a", "b", "c", "d"],
-    answer: "a",
+    title: "Commonly used data types DO NOT include:",
+    choices: ["A) alerts", "B) strings", "C) booleans", "D) numbers"],
+    answer: "A) alerts",
   },
   {
-    title: "Question 2",
-    choices: ["a", "b", "c", "d"],
-    answer: "b",
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["A) quotes", "B) curly brackets", "C) parentheses", "D) square brackets"],
+    answer: "C) parentheses",
   },
   {
-    title: "Question 3",
-    choices: ["a", "b", "c", "d"],
-    answer: "c",
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["A) numbers and strings", "B) other arrays", "C) booleans", "D) all of the above",],
+    answer: "D) all of the above",
   },
   {
-    title: "Question 4",
-    choices: ["a", "b", "c", "d"],
-    answer: "d",
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["A) commas", "B) curly brackets", "C) quotes", "D) parentheses"],
+    answer: "C) quotes",
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["A) JavaScript", "B) console.log", "C) terminal / bash", "D) for loops"],
+    answer: "B) console.log",
   },
 ];
